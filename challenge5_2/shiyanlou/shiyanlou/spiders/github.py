a8ee2766c9392a2cf42a24be060958afc87dc3c9
Yao-Phoenix <493867456@qq.com>
2020-03-09 17:26:07 +0800

    scrapy Github_data mit parse_start_url

---
+++ challenge5_2/shiyanlou/shiyanlou/spiders/github.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+import scrapy
+from shiyanlou.items import ShiyanlouItem
+from scrapy.linkextractors import LinkExtractor
+from scrapy.spider import Rule
+
+class GithubSpider(scrapy.spiders.CrawlSpider):
+    name = 'github'
+    allowed_domains = ['github.com']
+    start_urls = ['https://github.com/shiyanlou?tab=repositories']
+    
+    rules = (Rule(
+        LinkExtractor(
+            allow='https://github.com/shiyanlou\?after=*'),
+        callback='parse_item',
+        follow=True),
+#        Rule(
+#            LinkExtractor(
+#                allow='https://github.com/shiyanlou\?tab=repositories$'),
+#            callback='parse_item',
+#            follow=True),
+)
+
+    def parse_start_url(self, response):
+        for data in response.css('li.col-12'):
+            item = ShiyanlouItem()
+            item['repo_name'] = data.xpath('.//h3/a/text()').extract_first().strip()
+            item['update_time'] = data.xpath('.//relative-time/@datetime').extract_first()
+            yield item
+
+    def parse_item(self, response):
+        for data in response.css('li.col-12'):
+            item = ShiyanlouItem()
+            item['repo_name'] = data.xpath('.//h3/a/text()').extract_first().strip()
+            item['update_time'] = data.xpath('.//relative-time/@datetime'
+                    ).extract_first()
+            yield item
+            
