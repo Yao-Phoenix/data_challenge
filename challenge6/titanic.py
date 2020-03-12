ed5287889fe6ee6d18f8d2b5242e8b52d79b598e
Yao-Phoenix <493867456@qq.com>
2020-03-12 14:09:35 +0800

    titanic_data_seaborn

--- challenge6/titanic.py
+++ challenge6/titanic.py
@@ -4,10 +4,12 @@ import seaborn as sns
 
 def plot():
     df = sns.load_dataset('titanic')
-    df['age'].fillna(df['age'].median(), inplace=True)
+#    df['age'].fillna(df['age'].median(), inplace=True) 缺失值填充
+    sns.set_style('white') # 设置背景风格
     fig, axes = plt.subplots(1, 3, figsize=(15, 4))
     
-    sns.distplot(df['age'], ax=axes[0])
+    #删除缺失值
+    sns.distplot(df['age'].dropna(), ax=axes[0])
     sns.countplot(x='sex', hue='alive', data=df, ax=axes[1])
     sns.countplot(x='class', hue='alive', data=df, ax=axes[2])
     plt.show()
